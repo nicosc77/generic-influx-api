a7d218f8ab8570f9f3bbf040c0656c24ad2f6cab
Nicolas Schaber <nicolas.schaber@kit.edu>
2022-03-30 20:41:53 +0200

    Refactoring

--- mqtt-influx-bridge/Services/MetricService.cs
+++ mqtt-influx-bridge/Services/MetricService.cs
@@ -2,72 +2,83 @@ using InfluxDB.Client;
 using InfluxDB.Client.Api.Domain;
 using InfluxDB.Client.Writes;
 using mqtt_influx_bridge.Models;
+using static mqtt_influx_bridge.Utils.Constants;
 
 namespace mqtt_influx_bridge.Services;
 
 public class MetricService
 {
-    private readonly InfluxDBClient _influxDbClient;
+    private readonly string _bucket;
+    private readonly string _measurement;
+    private readonly string _org;
+    private readonly QueryApi _queryApi;
+    private readonly WriteApiAsync _writeApi;
 
     public MetricService(IConfiguration configuration)
     {
         var url = configuration["Influx:Url"];
         var token = configuration["Influx:Token"];
-        _influxDbClient = InfluxDBClientFactory.Create(url, token);
+        _org = configuration["Influx:Org"];
+        _measurement = configuration["Influx:measurement"];
+        _bucket = configuration["Influx:Bucket"];
+
+        var influxDbClient = InfluxDBClientFactory.Create(url, token);
+        _writeApi = influxDbClient.GetWriteApiAsync();
+        _queryApi = influxDbClient.GetQueryApi();
     }
 
     public async Task Write(GenericMetric metric)
     {
-        var writeApi = _influxDbClient.GetWriteApiAsync();
-        var builder = PointData.Measurement("GenericMeasurement").Tag("location", metric.Location)
+        var builder = PointData
+            .Measurement(_measurement)
+            .Tag(LocationTag, metric.Location)
             .Timestamp(DateTime.UtcNow, WritePrecision.Ns);
-        foreach (var (key, value) in metric.DataFeatures!)
-            builder = builder.Field($"{key}_type", value.Type).Field($"{key}_value", value.Value);
-        await writeApi.WritePointAsync(builder, "Metric", "Test");
+        metric.DataFeatures!.ToList().ForEach(entry =>
+        {
+            builder = builder.Field($"{entry.Key}{TypeKey}", entry.Value.Type)
+                .Field($"{entry.Key}{ValueKey}", entry.Value);
+        });
+        await _writeApi.WritePointAsync(builder, _bucket, _org);
     }
 
     public async Task<List<GenericMetric>> List()
     {
-        const string flux =
-            "from(bucket:\"Metric\") |> range(start: 0) |> pivot(rowKey:[\"_time\"], columnKey: [\"_field\"], valueColumn: \"_value\")";
-        var queryApi = _influxDbClient.GetQueryApi();
+        const string flux = "from(bucket:\"Metric\")\n"
+                            + " |> range(start: 0)"
+                            + " |> pivot(rowKey:[\"_time\"], columnKey: [\"_field\"], valueColumn: \"_value\")";
+
         var list = new List<GenericMetric>();
-        await queryApi.QueryAsync(flux, record =>
+        await _queryApi.QueryAsync(flux, record =>
+        {
+            var result = new GenericMetric();
+            result.Timestamp = (DateTime) record.GetTimeInDateTime()!;
+            result.DataFeatures = new Dictionary<string, GenericDataFeature>();
+            record.Values.ToList().ForEach(entry =>
             {
-                var result = new GenericMetric();
-                result.Timestamp = (DateTime) record.GetTimeInDateTime()!;
-                result.DataFeatures = new Dictionary<string, GenericDataFeature>();
-                record.Values.ToList().ForEach(entry =>
+                var fieldName = entry.Key.Split("_")[0];
+                if (entry.Key.EndsWith(TypeKey))
                 {
-                    var fieldName = entry.Key.Split("_")[0];
-                    if (entry.Key.EndsWith("_type"))
-                    {
-                        if (!result.DataFeatures!.ContainsKey(fieldName))
+                    if (!result.DataFeatures!.ContainsKey(fieldName))
+                        result.DataFeatures![fieldName] = new GenericDataFeature
                         {
-                            result.DataFeatures![fieldName] = new GenericDataFeature();
-                            result.DataFeatures![fieldName].Type = entry.Value.ToString();
-                        }
-                        else
+                            Type = entry.Value.ToString()
+                        };
+                    else
+                        result.DataFeatures![fieldName].Type = entry.Value.ToString();
+                }
+                else if (entry.Key.EndsWith(ValueKey))
+                {
+                    if (!result.DataFeatures!.ContainsKey(fieldName))
+                        result.DataFeatures![fieldName] = new GenericDataFeature
                         {
-                            result.DataFeatures![fieldName].Type = entry.Value.ToString();
-                        }
-                    }
-                    else if (entry.Key.EndsWith("_value"))
-                    {
-                        if (!result.DataFeatures!.ContainsKey(fieldName))
-                        {
-                            result.DataFeatures![fieldName] = new GenericDataFeature();
-                            result.DataFeatures![fieldName].Value = entry.Value.ToString();
-                        }
-                        else
-                        {
-                            result.DataFeatures![fieldName].Value = entry.Value.ToString();
-                        }
-                    }
-                });
-                list.Add(result);
-            },
-            error => { Console.WriteLine(error.ToString()); }, () => { Console.WriteLine("Query completed"); }, "Test");
+                            Value = entry.Value.ToString()
+                        };
+                    else
+                        result.DataFeatures![fieldName].Value = entry.Value.ToString();
+                }
+            });
+            list.Add(result);
+        }, error => throw new Exception(error.Message), () => { }, _org);
         return list;
     }
 }
---
+++ mqtt-influx-bridge/Utils/Constants.cs
@@ -0,0 +1,8 @@
+namespace mqtt_influx_bridge.Utils;
+
+public static class Constants
+{
+    public const string ValueKey = "_value";
+    public const string TypeKey = "_type";
+    public const string LocationTag = "location";
+}
